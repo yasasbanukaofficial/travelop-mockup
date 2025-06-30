7a2756f5f2255fc985888cd2a78d0f2f0d910229
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-07-01 00:54:29 +0530

    refactor: made the bg effect more darker

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,13 +2,14 @@ import LiquidChrome from "@/components/ui/LiquidChrome";
 
 export default function Home() {
   return (
-    <div className="fixed inset-0 -z-10 w-screen h-screen overflow-hidden">
+    <div className="relative w-screen h-screen overflow-hidden">
       <LiquidChrome
         baseColor={[0.0, 0.1, 0.1]}
         speed={0.3}
         amplitude={0.3}
         interactive={false}
       />
+      <div className="absolute inset-0 bg-black/60 pointer-events-none"></div>
     </div>
   );
 }
