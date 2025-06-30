7e26ed01a1ee6f202913e5eabf967c40e92fd3bf
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-07-01 00:46:12 +0530

    feat(ui): added background effect to main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,14 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import LiquidChrome from "@/components/ui/LiquidChrome";
 
 export default function Home() {
-  return <div className="main"># main body</div>;
+  return (
+    <div className="fixed inset-0 -z-10 w-screen h-screen overflow-hidden">
+      <LiquidChrome
+        baseColor={[0.0, 0.1, 0.1]}
+        speed={0.3}
+        amplitude={0.3}
+        interactive={false}
+      />
+    </div>
+  );
 }
